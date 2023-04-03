35e567a501bb3cf8dc7aee68fc2e0314cc3c22a2
Ivy-Chelsea <105067998+Ivy-Chelsea@users.noreply.github.com>
2023-04-03 21:54:46 +0300

    spawn

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
 """
-Method that spawns wait_random n times with
+Module that spawns wait_random n times with a
 specified delay between each call.
 """
 import asyncio
 from typing import List
+
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """Spawns wait_random n times with a specified delay
+    """
+    Spawns wait_random n times with a specified delay
     between each call.
     Args:
         n: number of times to spawn wait_random
